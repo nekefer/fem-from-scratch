deb52810c64f7958c166eacb15cc30cd96a6fda5
nekefer <kloutsedaniel1@gmail.com>
2024-11-14 14:02:25 -0600

    add mushroom-guide and some more html and css code

---
+++ main.js
@@ -0,0 +1,15 @@
+const hamburgerButton = document.querySelector(
+    '[aria-controls="primary-navigation"]'
+  );
+  const nav = document.querySelector(".primary-navigation");
+  
+  hamburgerButton.addEventListener("click", () => {
+    // check if the navigation is opened
+    const isNavOpened = hamburgerButton.getAttribute("aria-expanded");
+  
+    if (isNavOpened === "false") {
+      hamburgerButton.setAttribute("aria-expanded", "true");
+    } else {
+      hamburgerButton.setAttribute("aria-expanded", "false");
+    }
+  });``
